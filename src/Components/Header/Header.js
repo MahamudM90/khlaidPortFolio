91e8a616aa3a939e309418a39b1bd83e9ed34943
Md Khalid Hossain <zikaiwen02@gmail.com>
2021-05-03 04:53:07 +0800

    weaving hand added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,10 +8,18 @@ const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <h1 className="text-warning">this is my header</h1>
+                <h1 className="text-warning">
+                    Hi, I am <span style={{ color: "red" }}>Md Khalid Hossain</span>
+                    <p className="weave-imoji">👋</p>
+                </h1>
                 <Typed
                     className="react-typeing"
-                    strings={['web design', 'web development', 'facebook adds']}
+                    strings={[
+                        'I am looking for an internship',
+                        'I am a Frontend-Developer',
+                        'I am a Blog Writer',
+                        'I am a Youtuber'
+                    ]}
                     typeSpeed={40}
                     backSpeed={40}
                     loop
@@ -25,7 +33,7 @@ const Header = () => {
                     <a href="#" className="">
                         <button className="btn btn-lg btn-outline-warning">
                             <FontAwesomeIcon icon={faDownload} className="mr-2" />
-                            Contact me
+                            Download Resume
                         </button>
                     </a>
                 </div>
